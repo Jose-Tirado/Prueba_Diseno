2094b9364c85b0f808daa23c677b6a7948581257
Jose-Tirado <jtiradopeinado@correo.unicordoba.edu.co>
2022-07-12 16:55:16 -0500

    Summary

--- bin/Controller.js
+++ bin/Controller.js
@@ -6,65 +6,44 @@ const Artist = require("./models/Artist");
 
 class Controller {
     constructor() {
-        //al crearse el objeto se establece la conexion
+        
         this.connect();
     }
 
     async connect() {
         try {
-            //se intenta establecer una conexion con los datos de conexion
             await mongoose.connect(
-                "mongodb+srv://kikret:17DYcr6Zow5TYCSu@cluster0-9ek42.mongodb.net/test?retryWrites=true&w=majority",
-                //"mongodb://localhost:27017/my_music_collection_db",
-                { useNewUrlParser: true }
+                "mongodb+srv://admin:admin@cluster0.kxsse.mongodb.net/?retryWrites=true&w=majority",
+            { useNewUrlParser: true }
             );
-            //en caso de esablecer la conexion, se muestra en consola este mensaje
-            console.log("Connected databases.");
+                        console.log("Connected databases.");
         } catch (e) {
-            //en caso de haber un error se muestra en consola el error
-            console.error(e);
+                        console.error(e);
         }
     }
-    //CRUD https://coursework.vschool.io/mongoose-crud/
-    //CRUD https://www.callicoder.com/node-js-express-mongodb-restful-crud-api-tutorial/
 
-    //CRUD user
     setUser(user, res) {
-        // Se recibe el nuevo usuario en la variable user y se crea a partir del modelo
-        User.create(user, function(err, newUser) {
-            // sihay error se reporta
-            if (err) throw err;
-            // se retorna la informacion con el nuevo usuario creado
-            res.send({ status: 200, nU: newUser });
+                User.create(user, function(err, newUser) {
+                    if (err) throw err;
+                    res.send({ status: 200, nU: newUser });
         });
     }
-
-    //metodo para buscar todos los usuarios
     getUsers(req, res) {
-        //en el modelo User se ejecuta el find sin ninguna condicion...
         User.find({}, (err, users) => {
-            //en caso de haberse presentado un error se ejecuta el error
             if (err) throw err;
-            //de lo contrario se retorna un objeto con todos los resultados
             res.send({ allUsers: users });
         });
     }
 
     getUser(id, res) {
-        //en el modelo User se ejecuta el find sin ninguna condicion...
         User.find({ _id: id }, (err, user) => {
-            //en caso de haberse presentado un error se ejecuta el error
             if (err) throw err;
-            //de lo contrario se retorna un objeto con todos los resultados
             res.send({ User: user });
         });
     }
 
     updateUser(user, res) {
-        //optenemos los campos que queremos actualizar
         let { id, picture, password } = user;
-        //actualizamos teniendo en cuenta una condicion con el operador $set
-        //https://docs.mongodb.com/manual/reference/operator/update/set/
         User.updateOne(
             { _id: id },
             { $set: { picture: picture, password: password } }
@@ -82,10 +61,7 @@ class Controller {
             if (err) throw err;
             res.send({ message: "User has been deleted" });
         });
-    } // remove, findByIdAndRemove, findOneAndRemove
-
-    //CRUD list
-    setUserList(user_id, list, res) {
+    }     setUserList(user_id, list, res) {
         list.user_id = user_id;
         List.create(list, function(err, newList) {
             if (err) return handleError(err);
@@ -116,7 +92,6 @@ class Controller {
             });
     }
 
-    //CRUD song
     setSong(song, res) {
         Song.create(song, function(err, newSong) {
             if (err) throw err;
@@ -137,8 +112,6 @@ class Controller {
     }
 
     updateList(song_id, id_user, id_list, res) {
-        //actualizamos teniendo en cuenta una condicion con el operador $push
-        // https://docs.mongodb.com/manual/reference/operator/update/push/
         List.updateOne({ _id: id_list }, { $push: { song_id: song_id } })
             .then(rawResponse => {
                 res.send({ message: "List updated", raw: rawResponse });
@@ -148,7 +121,6 @@ class Controller {
             });
     }
 
-    //CRUD artist
     setArtist(artist, res) {
         Artist.create(artist, function(err, newArtist) {
             if (err) throw err;
@@ -171,8 +143,6 @@ class Controller {
     }
 
     updateSong(song_id, artists, res) {
-        //actualizamos teniendo en cuenta una condicion con el operador $push
-        // https://docs.mongodb.com/manual/reference/operator/update/push/
         Song.updateOne({ _id: song_id }, { $push: { artists: artists } })
             .then(rawResponse => {
                 res.send({ message: "Song updated", raw: rawResponse });
@@ -182,5 +152,4 @@ class Controller {
             });
     }
 }
-// se exporta elobjeto controlador crado a partir de la clase Controller
 exports.controller = new Controller();
